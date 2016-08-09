a86495e58d28e517b3e034c57aa086d1fee51dd1
James Carlson <jxxcarlson@gmail.com>
2016-08-09 11:06:39 -0400

    define version function in Global services

--- app/services/GlobalService.js
+++ app/services/GlobalService.js
@@ -3,6 +3,7 @@ module.exports = function() {
     // this.clientServer = function() { return "localhost:3000" }
     // this.apiServer = function() { return "localhost:2300"}
     
+    this.version = function() { return "1" }
     this.clientServer = function() { return "jxxmbp.local:3000" }
     this.apiServer = function() { return "jxxmbp.local:2300" }
     
