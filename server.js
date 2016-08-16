91faf1f46acdeb7814be389c8024e7b85c58a0d3
James Carlson <jxxcarlson@gmail.com>
2016-08-16 13:45:00 -0400

    x

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 http://expressjs.com/en/api.html
 http://shamadeh.com/blog/web/nodejs/express/2014/07/20/ExpressMultipleSites.html
 
+http://stackoverflow.com/questions/14125997/difference-between-app-all-and-app-use
 ************/
 
 var express = require('express')
@@ -10,10 +11,12 @@ var app = express()
 var PORT = 3000
 var requestCount = 0
 
+/*
 app.use(
     "/", //the URL throught which you want to access to you static content
     express.static(__dirname + '/public') //where your static content is located in your filesystem
 );
+*/
 
 app.all('*', function (req, res, next) {
   console.log('*** HOST: ' + req.hostname) 
@@ -22,14 +25,14 @@ app.all('*', function (req, res, next) {
         case 'www.manuscripta.io': res.sendFile('public/index.html', { root: __dirname }); 
               console.log('*** HANDLE MANUSCRIPTA'); break;
         case 'www.concertonovo.io': res.write('<h1>This is concertonovo.io</h1>'); 
-              console.log('*** HANLDE CONCERTONOVO'); res.end(); break;
+              console.log('*** HANDLE CONCERTONOVO'); res.end(); break;
         default: 
             res.statusCode = 404;
             res.write('<p>We do not serve the host: <b>' + req.hostname + '</b>.</p>'); res.end();
     }   
     
   requestCount += 1
-  console.log(requestCount + ' request: ' + req.params[0])
+  console.log('*** ' + requestCount + ' requests: ' + req.params[0])
 });
 
 app.listen(process.env.PORT || PORT) //the port you want to use
