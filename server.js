1ae05db23222ef60b5d1deb44feaea32276ac229
James Carlson <jxxcarlson@gmail.com>
2016-08-16 21:36:22 -0400

    Configure for production

--- server.js
+++ server.js
@@ -1,26 +1,40 @@
-/**********
+var express = require('express');
+var vhost = require('vhost');
 
-http://expressjs.com/en/api.html
+var concertoNovoDomain = 'www.concertonovo.io';
+var angularAppDomain = '*.manuscripta.io'
 
-************/
+var PORT = 3000
 
-var express = require('express')
 var app = express()
+var angularApp = express()
+var concertoNovoApp = express()
+
 var PORT = 3000
 var requestCount = 0
 
-app.use(
+angularApp.use(
     "/", //the URL throught which you want to access to you static content
     express.static(__dirname + '/public') //where your static content is located in your filesystem
 );
 
 // app.all('(?=^((?!style).)*$)(?=^((?!js).)*$)', function (req, res, next) {
-app.all('*', function (req, res, next) {
+angularApp.all('*', function (req, res, next) {
+  console.log('*** HOST: ' + req.hostname) 
+  console.log('*** PATH: ' + req.path)
   res.sendFile('public/index.html', { root: __dirname });
   requestCount += 1
-  console.log(requestCount + ' request: ' + req.params[0])
+  console.log(requestCount + ', request: ' + req.params[0])
 });
 
-app.listen(process.env.PORT || PORT) //the port you want to use
-console.log('\nServer listening at http://localhost:' + PORT + '\n');
+concertoNovoApp.use(
+    "/", //the URL throught which you want to access to you static content
+    express.static(__dirname + '/novo') //where your static content is located in your filesystem
+);
 
+app.use(vhost(concertoNovoDomain, concertoNovoApp))
+app.use(vhost(angularAppDomain, angularApp))
+
+app.listen(process.env.PORT || PORT, function() {
+    console.log( 'Express server listening on port %d in %s mode', (process.env.PORT || PORT), app.settings.env );
+});
