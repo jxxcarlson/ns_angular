e8eab136a1d5e57919648d88f0fbb222ca67e034
James Carlson <jxxcarlson@gmail.com>
2016-09-04 16:00:14 -0400

    x

--- public/js/main.js
+++ public/js/main.js
@@ -2564,57 +2564,60 @@ module.exports = function ($scope, UserService, UserApiService, DocumentApiServi
 
     UserApiService.getPreferences(self.username, self)
 
+    self.updateAclDisplay = function() {
 
-    var request = 'acl?acls_of_owner=' + UserService.username()
-    DocumentApiService.postRequest(request, $scope)
-        .then(function (response) {
+        var request = 'acl?acls_of_owner=' + UserService.username()
+        DocumentApiService.postRequest(request, $scope)
+            .then(function (response) {
 
-            var data = JSON.parse(response.data['acl_list'])
-            console.log('ACL DATA: ' + data)
-            console.log('ACL DATA LENGTH: ' + data.length)
-            console.log('ACL DATA first item: ' + data[0])
+                var data = JSON.parse(response.data['acl_list'])
+                console.log('ACL DATA: ' + data)
+                console.log('ACL DATA LENGTH: ' + data.length)
+                console.log('ACL DATA first item: ' + data[0])
 
-            var stringify_list = function(array) {
+                var stringify_list = function(array) {
 
-                var str = ""
-                array.forEach(function(element) { str += element + ', '})
-                return str.slice(0, str.length - 2)
-            }
+                    var str = ""
+                    array.forEach(function(element) { str += element + ', '})
+                    return str.slice(0, str.length - 2)
+                }
 
-            var stringify_pair_list = function(array) {
+                var stringify_pair_list = function(array) {
 
-                var str = ""
-                array.forEach(function(element) { str += element[1] + ', '})
-                return str.slice(0, str.length - 2)
-            }
+                    var str = ""
+                    array.forEach(function(element) { str += element[1] + ', '})
+                    return str.slice(0, str.length - 2)
+                }
 
-            var prettify = function(item) {
+                var prettify = function(item) {
 
-                var obj = {}
-                obj.name = item.name
-                obj.permission = item.permission
-                obj.members  = stringify_list(item.members)
-                obj.documents =  stringify_pair_list(item.documents)
-                return obj
-            }
+                    var obj = {}
+                    obj.name = item.name
+                    obj.permission = item.permission
+                    obj.members  = stringify_list(item.members)
+                    obj.documents =  stringify_pair_list(item.documents)
+                    return obj
+                }
 
-            var list = []
-            var counter = 0
+                var list = []
+                var counter = 0
 
-            data.forEach( function(item) {
+                data.forEach( function(item) {
 
-                list.push({
-                    "id": counter,
-                    "acl": prettify(item)
+                    list.push({
+                        "id": counter,
+                        "acl": prettify(item)
+                    })
                 })
+
+                console.log('*** acl list: ' + JSON.stringify(list))
+
+                self.aclList = list
+
             })
+    }
 
-            console.log('*** acl list: ' + JSON.stringify(list))
-
-            self.aclList = list
-
-        })
-
+    self.updateAclDisplay()
 
     console.log('foo = ' + self.foo)
     console.log('default doc type = ' + UserService.getPreferences().doc_format)
@@ -2643,28 +2646,57 @@ module.exports = function ($scope, UserService, UserApiService, DocumentApiServi
     self.addUserToAcl = function() {
 
         console.log('add user ' + self.usernameForAcl + ' to acl ' + self.aclNameForUser)
+
+        var request = 'acl?add_user=' + self.usernameForAcl +'&acl=' + self.aclNameForUser
+        DocumentApiService.postRequest(request, $scope)
+            .then( function() {self.updateAclDisplay()} )
+
+
     }
     self.removeUserFromAcl = function() {
 
         console.log('remove user ' + self.usernameForAcl + ' from acl ' + self.aclNameForUser)
+
+        var request = 'acl?remove_user=' + self.usernameForAcl +'&acl=' + self.aclNameForUser
+        DocumentApiService.postRequest(request, $scope)
+            .then( function() {self.updateAclDisplay()} )
     }
 
     self.addDocumentToAcl = function() {
 
         console.log('add document ' + self.documentIdForAcl + ' to acl ' + self.aclNameForDocument)
+
+        var request = 'acl?add_document=' + self.documentIdForAcl +'&acl=' + self.aclNameForDocument
+        console.log("*** request = " + request)
+        DocumentApiService.postRequest(request, $scope)
+            .then( function() {self.updateAclDisplay()} )
+
     }
     self.removeDocumentFromAcl = function() {
 
         console.log('remove document ' + self.documentIdForAcl + ' from acl ' + self.aclNameForDocument)
+
+        var request = 'acl?remove_document=' + self.documentIdForAcl +'&acl=' + self.aclNameForDocument
+        DocumentApiService.postRequest(request, $scope)
+            .then( function() {self.updateAclDisplay()} )
     }
 
     self.createAcl = function() {
 
         console.log('create ACL ' + self.aclNameCD + ' with permission ' + self.permissionForAcl)
-    }
+
+        var request = 'acl?create_acl=' + self.aclNameCD + '&permission=' + self.permissionForAcl
+        DocumentApiService.postRequest(request, $scope)
+            .then( function() {self.updateAclDisplay()} )
+
+}
     self.destroyAcl = function() {
 
         console.log('destroy ACL ' + self.aclNameCD)
+
+        var request = 'acl?remove_acl=' + self.aclNameCD
+        DocumentApiService.postRequest(request, $scope)
+            .then( function() {self.updateAclDisplay()} )
     }
 
 }
