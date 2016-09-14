346884673609e4de2c7d517d37536a2a72ceee79
James Carlson <jxxcarlson@gmail.com>
2016-09-14 00:53:04 -0400

    use &&

--- app/documents/services/DocumentApiService.js
+++ app/documents/services/DocumentApiService.js
@@ -123,8 +123,7 @@ module.exports = function ($http, $timeout, $q, $sce, $localStorage, $state, $lo
 
                     scope.tocTitle = 'Hotlist'
 
-                //} else if (DocumentService.parentId() != 0) {
-                } else if (false) {
+                } else if (DocumentService.parentId() != undefined && DocumentService.parentId() != 0) {
 
                     scope.tocTitle = 'Contents'
 
