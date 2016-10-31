67448b39a4f81a4e25946c4972e02284e0780fba
James Carlson <jxxcarlson@gmail.com>
2016-10-31 02:00:50 -0400

    x

--- public/js/main.js
+++ public/js/main.js
@@ -3228,7 +3228,7 @@ module.exports = function($scope, $http, $state, $location, $localStorage,
     }
 
     // console.log('EVENT: ' + JSON.stringify($event.currentTarget))
-    envService.set('production');
+    envService.set('development');
 
   // ABCDEF
 
