b0f24101ff5b90a8c1132539695d3be84f238e42
James Carlson <jxxcarlson@gmail.com>
2016-11-05 03:34:29 -0400

    Add target = "_self" to links in TOC

--- public/js/main.js
+++ public/js/main.js
@@ -3272,7 +3272,7 @@ module.exports = function($scope, $http, $state, $location, $localStorage,
     }
 
     // console.log('EVENT: ' + JSON.stringify($event.currentTarget))
-    envService.set('development');
+    envService.set('production');
 
   // ABCDEF
 
