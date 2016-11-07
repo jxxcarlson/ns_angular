efdf71a651359d9b954e1bfec99dbf4b07d02352
James Carlson <jxxcarlson@gmail.com>
2016-11-07 10:27:03 -0500

    heroku

--- app/topLevel/controllers/MainController.js
+++ app/topLevel/controllers/MainController.js
@@ -38,7 +38,7 @@ module.exports = function($scope, $http, $state, $location, $localStorage,
     }
 
     // console.log('EVENT: ' + JSON.stringify($event.currentTarget))
-    envService.set('development');
+    envService.set('production');
 
     $scope.refreshMathJax = function() {
 
