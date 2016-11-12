63e7524a34d293c39a047749c433a78bd4c91fb2
James Carlson <jxxcarlson@gmail.com>
2016-11-12 00:32:52 -0500

    merge with refactor and push to heroku

--- public/js/main.js
+++ public/js/main.js
@@ -3325,7 +3325,7 @@ module.exports = function($scope, $http, $state, $location, $localStorage,
     }
 
     // console.log('EVENT: ' + JSON.stringify($event.currentTarget))
-    envService.set('development');
+    envService.set('production');
 
 
 
