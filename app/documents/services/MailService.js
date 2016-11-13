c2b5da3289c782690237404c18a3b39aa5057616
James Carlson <jxxcarlson@gmail.com>
2016-11-12 22:14:53 -0500

    bug fix in mail service

--- app/documents/services/MailService.js
+++ app/documents/services/MailService.js
@@ -9,7 +9,7 @@ module.exports = function ($window, DocumentService) {
         var doc = DocumentService.document()
         var subject = doc.title // 'Manuscripta.io document'
         var message = 'You might be interested in%0D%0A%0D%0A        ' + doc.title + '%0D%0A%0D%0Aat http://www.manuscripta.io/documents/' + doc.id
-        message += "0D%0A%0D%0Ahttp://www.manuscripta.io is a site for creating and sharing documents online."
+        message += "%0D%0A%0D%0Ahttp://www.manuscripta.io is a site for creating and sharing documents online."
         message += "%0D%0AMathematics, Physics, Poetry, you name it."
         // this.send(recipient, subject, message)
         $window.open("mailto:" + "?subject=" + subject+"&body="+message,"_self");
