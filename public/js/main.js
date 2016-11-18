d0b404952a0a96ac69bd5e2c0067b1c15059ff09
James Carlson <jxxcarlson@gmail.com>
2016-11-18 08:24:21 -0500

    push heroku

--- public/js/main.js
+++ public/js/main.js
@@ -3375,7 +3375,7 @@ module.exports = function($scope, $http, $state, $location, $localStorage,
     }
 
     // console.log('EVENT: ' + JSON.stringify($event.currentTarget))
-    envService.set('development');
+    envService.set('production');
 
 
 
